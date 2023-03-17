592bb356ad4431bda3106477d86674e60ab096b2
n.yousefi <n.yousefi.w@gmail.com>
2023-03-17 17:58:29 +0330

    axes fix

--- site/js/chart-ease.js
+++ site/js/chart-ease.js
@@ -196,33 +196,37 @@
 
   function drawAxes(svg, axes, axesLines) {
     const g = createSVGElements("g");
+    const hStart = axes[0].axis.start;
+    const vStart = axes[1].axis.start;
+    const hStop = axes[0].axis.stop;
+    const vStop = axes[1].axis.stop;
     if (axesLines.left) {
       const axis = createAxis(g, axesLines.left);
-      axis.setAttribute("x1", axes[0].axis.start);
-      axis.setAttribute("x2", axes[0].axis.start);
-      axis.setAttribute("y1", axes[1].axis.start);
-      axis.setAttribute("y2", axes[1].axis.stop);
-    }
-    if (axesLines.top) {
-      const axis = createAxis(g, axesLines.top);
-      axis.setAttribute("x1", axes[0].axis.start);
-      axis.setAttribute("x2", axes[0].axis.stop);
-      axis.setAttribute("y1", axes[1].axis.stop);
-      axis.setAttribute("y2", axes[1].axis.stop);
-    }
-    if (axesLines.bottom) {
-      const axis = createAxis(g, axesLines.bottom);
-      axis.setAttribute("x1", axes[0].axis.start);
-      axis.setAttribute("x2", axes[0].axis.stop);
-      axis.setAttribute("y1", axes[1].axis.start);
-      axis.setAttribute("y2", axes[1].axis.start);
+      axis.setAttribute("x1", hStart);
+      axis.setAttribute("x2", hStart);
+      axis.setAttribute("y1", vStart);
+      axis.setAttribute("y2", vStop);
     }
     if (axesLines.right) {
       const axis = createAxis(g, axesLines.right);
-      axis.setAttribute("x1", axes[0].axis.stop);
-      axis.setAttribute("x2", axes[0].axis.stop);
-      axis.setAttribute("y1", axes[1].axis.start);
-      axis.setAttribute("y2", axes[1].axis.stop);
+      axis.setAttribute("x1", hStop);
+      axis.setAttribute("x2", hStop);
+      axis.setAttribute("y1", vStart);
+      axis.setAttribute("y2", vStop);
+    }
+    if (axesLines.top) {
+      const axis = createAxis(g, axesLines.top);
+      axis.setAttribute("x1", hStart);
+      axis.setAttribute("x2", hStop);
+      axis.setAttribute("y1", vStop);
+      axis.setAttribute("y2", vStop);
+    }
+    if (axesLines.bottom) {
+      const axis = createAxis(g, axesLines.bottom);
+      axis.setAttribute("x1", hStart);
+      axis.setAttribute("x2", hStop);
+      axis.setAttribute("y1", vStart);
+      axis.setAttribute("y2", vStart);
     }
     svg.appendChild(g);
   }
