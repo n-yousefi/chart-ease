e94d7af7b807db12f08b298f04d47499042395f4
n.yousefi <n.yousefi.w@gmail.com>
2023-10-10 19:41:57 +0330

    improve docs

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -42,15 +42,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/n-yousefi/chart-ease",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/n-yousefi/chart-ease",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -77,9 +75,9 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          // { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/n-yousefi/chart-ease",
             label: "GitHub",
             position: "right",
           },
@@ -117,13 +115,13 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/n-yousefi/chart-ease",
               },
             ],
           },
