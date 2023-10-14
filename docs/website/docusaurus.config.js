e75d5e2981019bee6184866fc384bc4121aa1619
n.yousefi <n.yousefi.w@gmail.com>
2023-10-14 18:52:39 +0330

    change social image

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/social.jpg",
       navbar: {
         title: "Chart Ease",
         logo: {
